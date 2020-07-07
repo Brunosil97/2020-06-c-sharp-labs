652ab4a589827cdc6bbbf1a9c84f8e448d68e477
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-07 15:50:18 +0100

    practiced with debugging in lab3

--- labs/lab_03_dot_net_core_hello_world/Program.cs
+++ labs/lab_03_dot_net_core_hello_world/Program.cs
@@ -10,7 +10,13 @@ namespace lab_03_dot_net_core_hello_world
             int[] myArray = {1,2,3,4};
             foreach(var item in myArray) {
                  Console.WriteLine(item);
-        } 
+        }
+            int total = 0;
+
+            for(int i=0; i < 5000; i++)
+            {
+                total += i;
+            }
         }
     }
 }
