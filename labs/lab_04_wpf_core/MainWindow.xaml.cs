350f2443a5c9285ffa04e5ebbf437862d1c6c4c0
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-07 14:52:13 +0100

    working on wpf, to create a calculator

--- labs/lab_04_wpf_core/MainWindow.xaml.cs
+++ labs/lab_04_wpf_core/MainWindow.xaml.cs
@@ -22,7 +22,43 @@ namespace lab_04_wpf_core
     {
         public MainWindow()
         {
+            //windows
             InitializeComponent();
+            LabelDisplay.Content = "0";
+        }
+
+        static string firstNumber = "0";
+        static string secondNumber = "0";
+        static string thirdNumber = "0";
+
+        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonEquals_Click(object sender, RoutedEventArgs e)
+        {
+            LabelDisplay.Content = firstNumber + secondNumber + thirdNumber;
+        }
+
+        private void ButtonEquals_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            LabelDisplay.Content = "Don't Double Click";
+        }
+
+        private void Button01_Click(object sender, RoutedEventArgs e)
+        {
+            firstNumber += "1";
+        }
+
+        private void Button02_Click(object sender, RoutedEventArgs e)
+        {
+            secondNumber += "2";
+        }
+
+        private void Button03_Click(object sender, RoutedEventArgs e)
+        {
+            thirdNumber += "3";
         }
     }
 }
