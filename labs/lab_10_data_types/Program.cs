690298c7d7d05b1e32a8f21db6f753035cfc17c0
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-08 13:48:11 +0100

    working with integers

---
+++ labs/lab_10_data_types/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab_10_data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int biggestValue = int.MaxValue;
+            long longBiggestValue = long.MaxValue;
+            //checked
+            //{
+            //    int a = Int32.MaxValue;
+            //    int b = a + 1;
+            //    Console.WriteLine(a);
+            //    Console.WriteLine(b);
+            //}
+
+            checked
+            {
+                float sum = 0;
+
+                for(int i = 0; i < 100; i++)
+                {
+                    sum += 1 / 2.0f;
+                }
+                Console.WriteLine("1/2 added 100 times: " + sum);
+                Console.WriteLine("1/2 multipled by 100: " + 1/3.0f *1000);
+            }
+        
+        }
+    }
+}
