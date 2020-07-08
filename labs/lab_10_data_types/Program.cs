8573ca8eb4b30215000720a43a2afabdceb0fb2b
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-08 15:19:12 +0100

    finished lab 10 with data types

--- labs/lab_10_data_types/Program.cs
+++ labs/lab_10_data_types/Program.cs
@@ -16,18 +16,51 @@ namespace lab_10_data_types
             //    Console.WriteLine(b);
             //}
 
-            checked
-            {
-                float sum = 0;
+            //checked
+            //{
+            //    float sum = 0;
 
-                for(int i = 0; i < 100; i++)
-                {
-                    sum += 1 / 2.0f;
-                }
-                Console.WriteLine("1/2 added 100 times: " + sum);
-                Console.WriteLine("1/2 multipled by 100: " + 1/3.0f *1000);
-            }
-        
+            //    for(int i = 0; i < 100; i++)
+            //    {
+            //        sum += 1 / 2.0f;
+            //    }
+            //    Console.WriteLine("1/2 added 100 times: " + sum);
+            //    Console.WriteLine("1/2 multipled by 100: " + 1/3.0f *1000);
+            //}
+
+            //float num = float.NaN;
+            //Console.WriteLine(num);
+
+            string bruno = "Bruno";
+            char firstLetter = 'b';
+            int firstLetterAsNumber = firstLetter;
+            Console.WriteLine(firstLetterAsNumber);
+
+            double x = 3.14;
+            float y = (float)x;
+            Console.WriteLine(y);
+
+            bool likeApples = true;
+            Console.WriteLine(likeApples);
+            double likeApplesAsDouble = System.Convert.ToInt32(likeApples);
+            Console.WriteLine(likeApplesAsDouble);
+
+            char initial = 'M';
+            int mCode = initial;
+            Console.WriteLine(mCode);
+            Console.WriteLine((char)mCode);
+
+            double pi = 3.141592;
+            pi = Math.Round(pi, 3);
+            Console.WriteLine(pi);
+
+            int num = -5;
+            ulong longNum = (ulong)num;
+            Console.WriteLine(longNum);
+
+            char four = '4';
+            int fourNum = four - '0';
+            Console.WriteLine(fourNum);
         }
     }
 }
