2464773532785247c5b3c74dd35c1e82a6b8672f
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-08 11:18:12 +0100

    able to read file as a string and in an array

--- labs/lab_09_logging/Program.cs
+++ labs/lab_09_logging/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 
 namespace lab_09_logging
 {
@@ -7,11 +8,25 @@ namespace lab_09_logging
     {
         static void Main(string[] args)
         {
-            File.WriteAllText("Output.log", $"Printing i");
+            File.WriteAllText("Output.log", $"Printing i\n\n");
             for(int i = 0; i < 10; i++)
             {
                 Console.WriteLine(i);
-                File.AppendAllText("Output.log", $"The value of i is {i}");
+                File.AppendAllText("Output.log", $"The value of i is {i} at {DateTime.Now}\n");
+                Thread.Sleep(500);
+            }
+            //read our log file
+            //1.Simple way - just all as one string
+            Console.WriteLine($"\n\nOutput as single string\n\n");
+            string outputAsString = File.ReadAllText("Output.log");
+            Console.WriteLine(outputAsString);
+
+            //2. Each line pushed into array
+            Console.WriteLine($"\n\nOutput as array\n\n");
+            string[] outPutArray = File.ReadAllLines("Output.log");
+            foreach (string line in outPutArray)
+            {
+                Console.WriteLine(line);
             }
         }
     }
