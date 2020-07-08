fa5dffce924973de1e72488e4e12f25f7070b011
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-08 11:41:22 +0100

    completed lab9

--- labs/lab_09_logging/Program.cs
+++ labs/lab_09_logging/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
+using System.Diagnostics;
 
 namespace lab_09_logging
 {
@@ -9,12 +10,26 @@ namespace lab_09_logging
         static void Main(string[] args)
         {
             File.WriteAllText("Output.log", $"Printing i\n\n");
+            #region loop with stopwatch
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+
             for(int i = 0; i < 10; i++)
             {
                 Console.WriteLine(i);
                 File.AppendAllText("Output.log", $"The value of i is {i} at {DateTime.Now}\n");
                 Thread.Sleep(500);
             }
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            Console.WriteLine(stopwatch.ElapsedTicks);
+
+            #endregion loop with stopwatch
+
             //read our log file
             //1.Simple way - just all as one string
             Console.WriteLine($"\n\nOutput as single string\n\n");
