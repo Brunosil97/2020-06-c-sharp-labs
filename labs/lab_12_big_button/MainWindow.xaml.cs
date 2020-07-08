55d858bde14e1d3657dc02d9ab3c6d4773989eee
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-08 17:00:15 +0100

    added text field, can create a folder within lab directory. But only one file is created

--- labs/lab_12_big_button/MainWindow.xaml.cs
+++ labs/lab_12_big_button/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
 
 namespace lab_12_big_button
 {
@@ -24,6 +26,7 @@ namespace lab_12_big_button
         public MainWindow()
         {
             InitializeComponent();
+            displayText.Text = "This took: 0s";
         }
 
         private void GoButton_Click(object sender, RoutedEventArgs e)
@@ -31,8 +34,19 @@ namespace lab_12_big_button
             var stopwatch = new Stopwatch();
             stopwatch.Start();
 
+            string folderName = @"C:\2020-06-labs\2020-06-c-sharp-labs\labs\lab_12_big_button";
+            string pathString = System.IO.Path.Combine(folderName, "textFolder");
+            System.IO.Directory.CreateDirectory(pathString);
 
+            for(int i = 0; i <= 100; i++)
+            {
+                string fileName = System.IO.Path.GetRandomFileName();
+                pathString = System.IO.Path.Combine(pathString, fileName);
+            }
+            
             stopwatch.Stop();
+
+            displayText.Text = $"This took: {stopwatch.Elapsed}s";
         }
     }
 }
