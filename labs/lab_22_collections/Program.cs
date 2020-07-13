e0973279b8b3afb8efa101f49a6b00a9ac56779e
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-13 14:58:02 +0100

    added a queue list

--- labs/lab_22_collections/Program.cs
+++ labs/lab_22_collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lab_22_collections
 {
@@ -6,7 +7,25 @@ namespace lab_22_collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declare a queue
+            var myQueue = new Queue<string>();
+            //add to the queue
+            myQueue.Enqueue("First item");
+            myQueue.Enqueue("Second item");
+            myQueue.Enqueue("Third item");
+            myQueue.Enqueue("Fourth item");
+
+            //look at the first item on the queue without removing
+            Console.WriteLine(myQueue.Peek());
+            //remove first item of a Queue
+            myQueue.Dequeue();
+            Console.WriteLine(myQueue.Peek());
+
+            //See if a queue contains a particular item
+            Console.WriteLine(myQueue.Contains("Third item")); //true or false
+
+            //declare a stack
+            var myStack = new Stack<string>();
         }
     }
 }
