ef05a99e9c9b9f8b325fecf1f6e404399cd811cd
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-13 14:08:51 +0100

    throwing exceptions and handliny them

--- labs/lab_21_exceptions/Program.cs
+++ labs/lab_21_exceptions/Program.cs
@@ -10,24 +10,42 @@ namespace lab_21_exceptions
         static void Main(string[] args)
         {
             //AddBeatle(4, "Brian");
+            //int x = 10;
+            //int y = 0;
+            //try { int output = x / y; } catch (Exception e) { Console.WriteLine("An exception has occured"); }
+            //finally { Console.WriteLine("But life goes on"); }
+
+            AddBeatle(4, "Brian"); 
         }
+        //public static void AddBeatle(int pos, string name)
+        //{
+        //    //Add a try and catch method
+        //    try
+        //    {
+        //        _theBeatles[pos] = name;
+        //    }
+        //    catch(Exception e) // if there is an error you can catch it, and display 
+        //    {
+        //        Console.WriteLine("Sorry, no 5th Beatle allowed");
+        //        Console.WriteLine($"{e.ToString()}");
+        //        Console.WriteLine($"{e.Message}");
+
+        //    }
+        //    finally //
+        //    {
+        //        Console.WriteLine("Here comes the sun");
+        //    }
+        //}
+
         public static void AddBeatle(int pos, string name)
         {
-            //Add a try and catch method
-            try
+            if(pos < 0 || pos >= _theBeatles.Length)
             {
-                _theBeatles[pos] = name;
+                throw new ArgumentException($"The beatles do have a position {pos}");
             }
-            catch(Exception e) // if there is an error you can catch it, and display 
-            {
-                Console.WriteLine("Sorry, no 5th Beatle allowed");
-            }
-            finally //
-            {
-                Console.WriteLine("Here comes the sun");
-            }
+            _theBeatles[pos] = name;
         }
 
-        
+
     }
 }
