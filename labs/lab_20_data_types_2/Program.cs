83cc63d22857ab40ddc78ed2f5a4645321ca6d03
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-13 11:36:29 +0100

    working on enums

--- labs/lab_20_data_types_2/Program.cs
+++ labs/lab_20_data_types_2/Program.cs
@@ -24,7 +24,53 @@ namespace lab_20_data_types_2
             //add month
             d = d.AddMonths(1);
 
+            //birthday
+            var brunoBday = new DateTime(1997, 04, 18);
+            var age = DateTime.Now - brunoBday;
 
+            int age2 = (int)((age.Days) / 365.25);
+            Console.WriteLine(age2);
+
+            var date = DateTime.Now.ToString("dd-MM-yyyy");
+            var date2 = brunoBday.ToString("yy/MM/dd");
+            Console.WriteLine(date);
+            Console.WriteLine(date2);
+
+            //1 hour time span
+            var timespan = new TimeSpan(1, 0, 0);
+            //add the time right now
+            var date3 = DateTime.Now + timespan;
+            // add one tick to our date
+            var tick = new TimeSpan(1);
+            date3 = date3 + tick;
+
+            Suit theSuit = Suit.HEARTS;
+            int suit = (int)theSuit;
+            Console.WriteLine(suit);
+            theSuit = Suit.DIAMONDS;
+
+            switch (theSuit)
+            {
+                case (Suit.HEARTS):
+                    Console.WriteLine("Heart");
+                    break;
+                case (Suit.SPADES):
+                    Console.WriteLine("Spade");
+                    break;
+                case (Suit.DIAMONDS):
+                    Console.WriteLine("Diamond");
+                    break;
+                case (Suit.CLUBS):
+                    Console.WriteLine("Club");
+                    break;
+
+            }
+
+        }
+
+        public enum Suit
+        {
+            HEARTS, CLUBS, DIAMONDS, SPADES
         }
     }
 }
