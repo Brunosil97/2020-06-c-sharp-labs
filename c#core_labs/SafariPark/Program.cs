a29cdf3023c79bfde0bc90bf2d8648c89ba7a49b
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-14 15:07:17 +0100

    working with structs and now beginning with tests

--- c#core_labs/SafariPark/Program.cs
+++ c#core_labs/SafariPark/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Net.Cache;
 
 namespace SafariPark
 {
@@ -18,7 +20,21 @@ namespace SafariPark
             //overloading constructor instance
             //Person luis = new Person("Luis", "Wolton", 23);
             //Console.WriteLine(luis.Age);
+
+            //structs 
+            var s1 = new Point3D() { x = 1, y = 2, z = 3 };
+
+            Person john = new Person("John", "Jones") { Age = 20 };
+            Point3D pt3d = new Point3D(5, 8, 2);
+
+            DemoMethod(pt3d, john);
+          
         }
 
+        static void DemoMethod(Point3D pt, Person p)
+        {
+            pt.y = 1000;
+            p.Age = 92;
+        }
     }
 }
