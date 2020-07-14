d2da5c51bdc8d20f5b9fbbf289aa60f058beeb99
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-14 09:45:44 +0100

    solutions for collections

--- CollectionsExercises/CollectionsExercisesTests/AverageTests.cs
+++ CollectionsExercises/CollectionsExercisesTests/AverageTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+<<<<<<< HEAD
 
 namespace CollectionsExercisesTests
 {
@@ -15,4 +16,28 @@ namespace CollectionsExercisesTests
 			Assert.Pass();
 		}
 	}
+=======
+using CollectionsExercisesLib;
+using System.Collections.Generic;
+
+namespace CollectionsExercisesTests
+{
+    public class AverageTests
+    {
+        [Test]
+        public void IfTheListIsEmptyTheAverageIsZero()
+        {
+            var argList = new List<double>();
+            var result = ListExercises.Average(argList);
+            Assert.AreEqual(0, result);
+        }
+        [Test]
+        public void GivenAListIsTheAverageCorrect()
+        {
+            var argList = new List<double> { 5, 2, 6, 1 };
+            var result = ListExercises.Average(argList);
+            Assert.AreEqual(3.5, result);
+        }
+    }
+>>>>>>> collections_hw
 }
