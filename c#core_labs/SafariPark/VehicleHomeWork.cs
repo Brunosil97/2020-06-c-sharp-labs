22e335e17fee043b19064a38fae811bb5e5675c0
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-14 16:16:25 +0100

    completed vehicle homework

---
+++ c#core_labs/SafariPark/VehicleHomeWork.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafariPark
+{
+    public class Vehicle
+    {
+        private int _capacity;
+        private int _numPassengers;
+        private int _speed;
+
+        public int NumPassengers
+        {
+            get { return _numPassengers; }
+            set { if (value < _capacity || value >= 0) _numPassengers = value; }
+        }
+        public int Position { get; private set; }
+
+        public Vehicle(int capacity, int speed = 10)
+        {
+            _capacity = capacity;
+            _speed = speed;
+           
+        }
+
+        public Vehicle()
+        {
+            _speed = 10;
+        }
+        public string Move(int times)
+        {
+            Position += _speed * times;
+            return $"Moving along {times} times";
+        }
+        public string Move()
+        {
+            Position += _speed;
+            return "Moving along";
+        }
+    }
+}
---
+++ c#core_labs/SafariParkTest/VehicleHomeWorkTests.cs
@@ -0,0 +1,33 @@
+﻿
+using NUnit.Framework;
+using SafariPark;
+
+namespace SafariParkTest
+{
+    class VehicleHomeWorkTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void WhenADefaultVehicleMovesTwiceItsPositionIs20()
+        {
+            Vehicle v = new Vehicle();
+            var result = v.Move(2);
+            Assert.AreEqual(20, v.Position);
+            Assert.AreEqual("Moving along 2 times", result);
+            
+        }
+
+        [Test]
+        public void WhenAVehicleWithSpeed40IsMoveOnceItsPositionIs40()
+        {
+            Vehicle v = new Vehicle(5, 40);
+            var result = v.Move();
+            Assert.AreEqual(40, v.Position);
+            Assert.AreEqual("Moving along", result);
+        }
+    }
+}
