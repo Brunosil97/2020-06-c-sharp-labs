bb5ce4969cb83d21249b15a207c8ec048314a7dc
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-20 15:39:55 +0100

    completed walkthrough of the MDN queries

---
+++ c#data/CSharpSQLStarter/QueryMDNWalkthrough/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueryMDNWalkthrough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
