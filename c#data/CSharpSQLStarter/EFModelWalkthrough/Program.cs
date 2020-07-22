7644d111401f49981c001b9e5af3a308f5a40ecd
Bruno Silva <brunosilvinosilva@gmail.com>
2020-07-22 13:54:22 +0100

    added new instances to database and done simple query

--- c#data/CSharpSQLStarter/EFModelWalkthrough/Program.cs
+++ c#data/CSharpSQLStarter/EFModelWalkthrough/Program.cs
@@ -10,9 +10,22 @@ namespace EFGetStarted
             using (var db = new BloggingContext())
             {
                 // Create
-                Console.WriteLine("Inserting a new blog");
-                db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
-                db.SaveChanges();
+                //Console.WriteLine("Inserting a new blog");
+                //db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                //db.SaveChanges();
+
+                //var blog1 = new Blog();
+                //blog1.Url = "https://devblogs.microsoft.com/dotnet";
+                //db.Add(blog1);
+                //db.SaveChanges();
+
+                //blog1.Posts.Add(
+                //    new Post
+                //    {
+                //        Title = "This is my post",
+                //        Content = "I am adding a new post"
+                //    });
+
 
                 // Read
                 Console.WriteLine("Querying for a blog");
@@ -31,10 +44,27 @@ namespace EFGetStarted
                     });
                 db.SaveChanges();
 
-                // Delete
-                //Console.WriteLine("Delete the blog");
+                ////delete
+                //Console.WriteLine("delete the blog");
                 //db.Remove(blog);
                 //db.SaveChanges();
+
+
+
+                var query =
+                    from blogs in db.Blogs
+                    join post in db.Posts on blogs.BlogId equals post.BlogId
+                    select new
+                    {
+                        BlogId = blogs.BlogId,
+                        Url = blogs.Url,
+                        Content = post.Content
+                    };
+
+                foreach (var post in query)
+                {
+                    Console.WriteLine($"{post.BlogId} - {post.Url} - {post.Content}");
+                }
             }
         }
     }
