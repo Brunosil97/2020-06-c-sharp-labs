7f3269c25f6ae3f85c46703d1a5e7188459da175
Bruno Silva <brunosilvinosilva@gmail.com>
2020-08-03 14:51:11 +0100

    created labs to work with delegates, and how to use them

---
+++ labs/lab_26_delegates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace lab_26_delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Delegates
+
+            //1. Placeholder for one or more methods
+            //2. Can use in annoymous 'lambda' expressions, very usefull particually with 'async' web calls
+            //3. Link to events ==> EVENT fires eg button click ==> all methods must match pattern given in our delegate
+            //               void myDelegate01(); any methods would have to match pattern void myMethod();
+            //               string          (int)                                       string          (int)
+
+
+            //Create an event ==> and run given methods
+            //1. Create Delegate
+            //2. Create delegate instance
+            var delegateInstance = new MyDelegate01(Method01);
+
+            //3. Run the delegate instance and will call the method
+            delegateInstance();
+        }
+
+
+        delegate void MyDelegate01(); //Maatches method of void and no parameters
+
+        //These are Action methods: Don't return anything or take anything
+
+        static void Method01()
+        {
+            Console.WriteLine("I am Method01");
+        }
+
+        static void Method02()
+        {
+            Console.WriteLine("I am Method02");
+        }
+    }
+}
---
+++ labs/lab_27_delegates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace lab_27_delegates
+{
+    class Program
+    {
+        delegate void Delegate01();
+
+        delegate string Delegate02(int x, bool y);
+        static void Main(string[] args)
+        {
+            //shorter version
+            // notice no brackets in method => just a placeholder, dont call it right now
+            Delegate01 delegateInstance = Method01;
+
+            Action myOtherDelegateInstance = Method02;
+
+            Delegate02 stringInstance = StringMethod;
+
+            //run
+            delegateInstance();
+            myOtherDelegateInstance();
+            Console.WriteLine(stringInstance(2, true));
+        }
+
+        static void Method01()
+        {
+            Console.WriteLine("Running Method  01");
+        }
+
+        static void Method02()
+        {
+            Console.WriteLine("Running Method  02");
+        }
+
+        static string StringMethod(int x, bool y)
+        {
+            if (x % 2 == 0 && y == true)
+            {
+                return $"{x} is even";
+            }
+            else
+            {
+                return $"{x} is not even";
+            }
+            
+        }
+    }
+}
