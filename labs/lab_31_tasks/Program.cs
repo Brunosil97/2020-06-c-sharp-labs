e6e57853a37e49fa2e11382f945e72b0b9aa18a4
Bruno Silva <brunosilvinosilva@gmail.com>
2020-08-04 10:49:49 +0100

    introduction to background tasks

---
+++ labs/lab_31_tasks/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab_31_tasks
+{
+    class Program
+    {
+        static Stopwatch s = new Stopwatch();
+        static void Main(string[] args)
+        {
+            /*
+             Tasks:
+            
+            Async - Main() thread
+
+            Sync - 
+
+            Process - exe running application which is able to send commands to CPU for processing
+                Thread ==> set of instructions bundled up and sent to CPU for processing
+                Main(){
+                    subThread() ==> execute as different process computer
+                }
+
+            C#
+                Threading - manually created thread
+
+                Tasks ==> hard work removed ==> easy for programmer to work with sub threads/ sub tasks
+
+            Key words:
+                Process          - running exe
+                Application      - run by user and runs in foreground
+                Service          - run by computer at start up and runs in background (DNS, VPN. website)
+                Thread           - set of instructions sent to CPU for processing
+                Single-Threaded  - Runs on the main thread only
+                Multi-Threaded   - Can take advantage of multi core CPUs which can run multiple threads simultaneously
+            */
+
+            s.Start();
+
+            var thread = new Thread(
+                () => {
+                    Thread.Sleep(3000);
+                    Console.WriteLine($"This is a thread {s.ElapsedMilliseconds}"); }
+                );
+
+
+            var task01 = new Task(
+                () =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"This is a task {s.ElapsedMilliseconds}");
+                });
+
+                //create and run at same time
+            var task02 = new Task(
+                () => {
+                    Thread.Sleep(2000);
+                    Console.WriteLine($"This is task 2 {s.ElapsedMilliseconds}");
+                }
+                );
+            s.Start();
+
+            thread.Start();
+
+            task01.Start();
+
+            task02.Start();
+
+            Console.WriteLine($"Program has ended {s.ElapsedMilliseconds}");
+            Console.ReadLine();
+        }
+    }
+}
