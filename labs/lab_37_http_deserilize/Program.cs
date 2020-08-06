c7cd87bfea11935f8189efd373afca9ada338868
Bruno Silva <brunosilvinosilva@gmail.com>
2020-08-06 09:21:38 +0100

    retrieved data from api and displayed on wpf

--- labs/lab_37_http_deserilize/Program.cs
+++ labs/lab_37_http_deserilize/Program.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using System.Threading;
+using System.Runtime.ConstrainedExecution;
+using System.IO;
 
-namespace lab_37_http_deserilize
+namespace premierLeagueApi
 {
     public class Todo
     {
@@ -18,33 +20,138 @@ namespace lab_37_http_deserilize
         public bool Completed { get; set; }
 
     }
-    class Program
+
+    public class Area
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Competition
+    {
+        public int id { get; set; }
+        public Area area { get; set; }
+        public string name { get; set; }
+        public string code { get; set; }
+        public string plan { get; set; }
+        public DateTime lastUpdated { get; set; }
+    }
+
+    public class Season
+    {
+        public int id { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public int currentMatchday { get; set; }
+        public object winner { get; set; }
+    }
+
+    public class Team
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string crestUrl { get; set; }
+
+        public override string ToString()
+        {
+            return $"{name}";
+        }
+    }
+
+    public class Table
+    {
+        public int position { get; set; }
+        public Team team { get; set; }
+        public int playedGames { get; set; }
+        public int won { get; set; }
+        public int draw { get; set; }
+        public int lost { get; set; }
+        public int points { get; set; }
+        public int goalsFor { get; set; }
+        public int goalsAgainst { get; set; }
+        public int goalDifference { get; set; }
+
+    }
+
+    public class Standing
+    {
+        public string stage { get; set; }
+        public string type { get; set; }
+        public object group { get; set; }
+        public List<Table> table { get; set; }
+    }
+
+    public class Root
+    {
+        public Filters filters { get; set; }
+        public Competition competition { get; set; }
+        public Season season { get; set; }
+        public List<Standing> standings { get; set; }
+    }
+
+    public class Filters
+    {
+
+    }
+    public class ApiRequests
     {
         static Uri todosUrl = new Uri("https://jsonplaceholder.typicode.com/todos");
 
+        static Uri premierLeagueUrl = new Uri("http://api.football-data.org/v2/competitions/2021/standings");
+
         static List<Todo> todos = new List<Todo>();
         static List<Todo> todosAsync = new List<Todo>();
+        public List<Team> premierLeagueTeams = new List<Team>();
+        public Root premierLeague;
         static void Main(string[] args)
         {
             //Sync
-            GetTodos();
+            //GetTodos();
 
-            Console.WriteLine($"Synchronously we have obtained {todos.Count} records");
+            //Console.WriteLine($"Synchronously we have obtained {todos.Count} records");
 
-            //Async
-            var s = new Stopwatch();
-            s.Start();
-            GetTodosAsync();
+            ////Async
+            //var s = new Stopwatch();
+            //s.Start();
+            //GetTodosAsync();
 
-            Console.WriteLine($"Asynchronously we have obtained {todosAsync.Count} records at {s.ElapsedMilliseconds}");
+            //Console.WriteLine($"Asynchronously we have obtained {todosAsync.Count} records at {s.ElapsedMilliseconds}");
 
-            Thread.Sleep(2000);
-            Console.WriteLine($"Asynchronously we have obtained {todosAsync.Count} records at {s.ElapsedMilliseconds}");
-            s.Stop();
+            //Thread.Sleep(2000);
+            //Console.WriteLine($"Asynchronously we have obtained {todosAsync.Count} records at {s.ElapsedMilliseconds}");
+            //s.Stop();
+            
+           
         }
 
+        public void GetTeams()
+        {
+            GetCompetition();
+            foreach (var standing in premierLeague.standings)
+            {
+                if (standing.type == "TOTAL")
+                {
+                    foreach (var table in standing.table)
+                    {
+                        premierLeagueTeams.Add(table.team);
+                    }
+                }
+            }
+        }
 
-        static void GetTodos()
+        public void GetCompetition()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("X-Auth-Token", "04ffe7eb763b42f1805c7a2857c8238d");
+                var response =  httpClient.GetStringAsync(premierLeagueUrl);
+                var data = response.Result;
+                
+
+                premierLeague = JsonConvert.DeserializeObject<Root>(data);
+            }
+        }
+        public static void GetTodos()
         {
             using (var httpClient = new HttpClient())
             {
@@ -55,7 +162,7 @@ namespace lab_37_http_deserilize
             }
         }
 
-        static async void GetTodosAsync()
+        public static async void GetTodosAsync()
         {
             using(var httpClient = new HttpClient())
             {
---
+++ labs/lab_38_wpf_api/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using premierLeagueApi;
+
+namespace lab_38_wpf_api
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApiRequests _apiRequests = new ApiRequests();
+        Standing standing = new Standing();
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateListBox();
+        }
+
+        public void PopulateListBox()
+        {
+            
+            _apiRequests.GetTeams();
+            apiListBox.ItemsSource = _apiRequests.premierLeagueTeams;
+        }
+    }
+}
