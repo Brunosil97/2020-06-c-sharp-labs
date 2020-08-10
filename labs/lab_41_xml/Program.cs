5d34e08bc12766e7e969b391c43a13e32660c9d9
Bruno Silva <brunosilvinosilva@gmail.com>
2020-08-10 16:12:48 +0100

    working on deserlizing xml

--- labs/lab_41_xml/Program.cs
+++ labs/lab_41_xml/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace lab_41_xml
@@ -19,14 +20,22 @@ namespace lab_41_xml
                     new XElement("level2", 400)
                 ),
                 new XElement("level1",
-                    new XElement("level 11", 100),
-                    new XElement("level 12", 100)
+                    new XElement("level1.1", 100),
+                    new XElement("level1.2", 100)
                 )
             );
-
-
-
             Console.WriteLine(xml02);
+
+            //write data to disk
+            var doc02 = new XDocument(xml02);
+            doc02.Save("XMLdoc02.xml");
+            //read
+
+            var readDoc02 = new XmlDocument();
+            readDoc02.Load("XMLdoc02.xml");
+
+            //print
+            Console.WriteLine(readDoc02.InnerXml);
         }
     }
 }
---
+++ labs/lab_42_xml_serilize/Program.cs
@@ -0,0 +1,64 @@
+﻿using lab_42_xml_serilize.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace lab_42_xml_serilize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            //read some northwind products
+            using(var db = new NorthwindContext())
+            {
+                products = db.Products.Take(5).ToList();
+            }
+            products.ForEach(p => Console.WriteLine($"{p.ProductId} {p.ProductName}"));
+
+
+            var xmlProducts = new XElement(
+                "Products",
+                from p in products
+                select new XElement("Product",
+                    new XElement("ProductID", p.ProductId),
+                    new XElement("ProductName", p.ProductName),
+                    new XElement("UnitPrice", p.UnitPrice)
+                    )
+                );
+
+            //save document
+            var xmlProductDoc = new XDocument(xmlProducts);
+            xmlProductDoc.Save("Products.xml");
+
+            //print
+            Console.WriteLine(xmlProductDoc);
+
+            //assume data being sent to us over the internet
+            //safest to stream
+            var xmlProducts2 = new Products();
+
+            using(var reader = new StreamReader("Products.xml"))
+            {
+                //deserilize from xml into northwind products
+                var serializer = new XmlSerializer(typeof(Products));
+                xmlProducts2 = (Products)serializer.Deserialize(reader);
+            };
+        }
+    }
+
+    [XmlRoot("Products")]
+    public class Products
+    {
+        [XmlElement("Product")]
+        public List<Product> XMLProductsFromBrazil { get; set; }
+    }
+}
