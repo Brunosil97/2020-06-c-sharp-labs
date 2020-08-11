301b4b533b1c8c1eed99683e1e388f3ef8fecad9
Bruno Silva <brunosilvinosilva@gmail.com>
2020-08-11 20:15:45 +0100

    have api backend for my frontend

---
+++ labs/GameBackend/Migrations/20200811190319_addConsole.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameBackend.Migrations
+{
+    public partial class addConsole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Rating = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Console = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.GameId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+        }
+    }
+}
---
+++ labs/GameBackend/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameBackend.Models
+{
+    public partial class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public string Description { get; set; }
+        public string Console { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
