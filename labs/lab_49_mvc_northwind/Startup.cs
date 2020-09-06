be20c8ff0669603114a4fb9b3bb125c13600e5e8
Bruno Silva <brunosilvinosilva@gmail.com>
2020-09-06 13:48:54 +0100

    completed lab49 and now starting tech prep

--- labs/lab_49_mvc_northwind/Startup.cs
+++ labs/lab_49_mvc_northwind/Startup.cs
@@ -25,7 +25,7 @@ namespace lab_49_mvc_northwind
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AzureNorthwind")));
+            services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Northwind")));
             services.AddControllersWithViews();
         }
 
