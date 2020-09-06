8f5cc2dac9b41de3de0bb849e8065d6b05742c1c
Bruno Silva <brunosilvinosilva@gmail.com>
2020-09-06 13:53:11 +0100

    completed first question

--- TechInterviewPrep/TechInterviewQuestions/Program.cs
+++ TechInterviewPrep/TechInterviewQuestions/Program.cs
@@ -6,7 +6,21 @@ namespace TechInterviewQuestions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tech Interview!");
+
+            Console.WriteLine("Quick Fire: 1");
+            FizzBuzz(100);
+        }
+
+        static void FizzBuzz(int number)
+        {
+            for(int i = 0; i < number; i++)
+            {
+                if (i % 15 == 0) { Console.WriteLine("FizzBuzz"); }
+                else if (i % 5 == 0) { Console.WriteLine("Buzz"); }
+                else if (i % 3 == 0) { Console.WriteLine("Fizz"); }
+                else { Console.WriteLine(i); }
+            }
         }
     }
 }
